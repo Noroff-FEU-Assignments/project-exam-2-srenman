7e6cdc9d8faf8233105dcec46f2af2a4f99843c3
srenman <48174060+srenman@users.noreply.github.com>
2021-05-18 07:36:07 +0800

    Obituary layout dynamic

--- layouts/obituary.vue
+++ layouts/obituary.vue
@@ -13,21 +13,27 @@
         </div>
 
         <div class="p-2 w-3/5">
-          <h2 class="text-lg font-bold">Karl Olav Anker Johansen</h2>
-          <p class="font-semibold">1.4.1928 - 23.12.2020</p>
-          <p>fra <Bold>Borge Kirke</Bold> til <Bold>Vik Kirkegård </Bold></p>
+          <h2 class="text-lg font-bold">{{ person.name }}</h2>
+          <p class="font-semibold">
+            {{ person.person_information.birthday }} -
+            {{ person.person_information.deceased }}
+          </p>
+          <p>
+            fra <Bold>{{ person.funeral_information.church }}</Bold> til
+            <Bold> {{ person.funeral_information.cementary }} </Bold>
+          </p>
 
-          <p class="font-semibold">8. januar 2020 kl 10.30</p>
+          <p class="font-semibold">{{ person.funeral_information.date }}</p>
         </div>
       </div>
       <div class="my-10">
         <nuxt-link
-          to="/obituary"
+          :to="`/obituary/${person.id}`"
           class="bg-primary-500 px-6 py-2 rounded-md hover:bg-primary-700 text-white mx-2"
           >Kondolanser</nuxt-link
         >
         <nuxt-link
-          to="/obituary/funeral"
+          :to="`/obituary/${person.id}/funeral`"
           class="bg-primary-500 px-6 py-2 rounded-md hover:bg-primary-700 text-white"
           >Begravelse</nuxt-link
         >
@@ -46,6 +52,28 @@ import Bold from '../components/ui/typography/Bold'
 export default {
   components: {
     Bold,
+  },
+  data() {
+    return {
+      person: {
+        id: '123',
+        name: 'Ola Nordmann',
+        status: 'Aktiv',
+        person_information: {
+          birthday: '1934.04.23',
+          deceased: '2021.04.26',
+        },
+        funeral_information: {
+          church: 'Borge Kirke',
+          cementary: 'Vik kirkegård',
+          date: '8. Januar 2021 kl. 10.30',
+        },
+        bg_image_id:
+          'https://images.unsplash.com/photo-1474533883693-59a44dbb964e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80',
+        person_image_id:
+          'https://images.unsplash.com/photo-1544819576-82e8d26e7d22?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
+      },
+    }
   },
 }
 </script>
